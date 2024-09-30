06ddeedac7632c18108250f755b287832b031923
ikemh <ikemh@hotmail.com>
2024-09-30 19:47:40 -0300

    refactore js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.querySelector("header");
+  const headerContainer = document.querySelector(".header-container");
+  const contactSection = document.getElementById("contato");
 
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > 70) {
-      header.classList.add("sticky-active");
-    } else {
-      header.classList.remove("sticky-active");
-    }
-  });
+  initScrollEvents(header);
+  initPhoneInputMasking();
+  initSmoothScrollForMenuLinks();
+  initCarousel();
+  initHeroSectionAnimation();
+  initParallaxEffect();
+  initFormSubmissionHandling(headerContainer, contactSection);
+  initImageTogglingOnMobile();
+  initWhatsAppButtonVisibility();
+  initHighlightAnimation();
 });
 
-const input = document.querySelector("#phone");
-const iti = window.intlTelInput(input, {
-  onlyCountries: ["br", "us", "ar", "uy", "py"],
-  initialCountry: "br",
-  preferredCountries: [],
-  separateDialCode: true,
-  utilsScript:
-    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js", // Para utilidades adicionais, como validação
-});
-
-document.addEventListener("DOMContentLoaded", function () {
+function initScrollEvents(header) {
+  let lastScrollTop = 0;
   const menuItems = document.querySelectorAll(".menu-ancora a");
   const sections = document.querySelectorAll("section");
 
-  function onScroll() {
-    let scrollPos =
-      window.scrollY ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop;
+  function updateMenuActiveState() {
+    const scrollPos = window.scrollY || document.documentElement.scrollTop;
 
     sections.forEach((section, index) => {
       if (
@@ -36,13 +29,11 @@ document.addEventListener("DOMContentLoaded", function () {
         scrollPos < section.offsetTop + section.offsetHeight
       ) {
         menuItems.forEach((item, i) => {
-          item.parentElement.classList.remove("active");
-          item.parentElement.classList.remove("completed");
+          item.parentElement.classList.remove("active", "completed");
 
           if (item.getAttribute("href").substring(1) === section.id) {
             item.parentElement.classList.add("active");
 
-            // Adiciona a classe "completed" aos itens anteriores
             for (let j = 0; j < i; j++) {
               menuItems[j].parentElement.classList.add("completed");
             }
@@ -52,26 +43,62 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  window.addEventListener("scroll", onScroll);
-  onScroll(); // Chama a função para definir a seção ativa ao carregar a página
-});
+  window.addEventListener("scroll", function () {
+    const currentScroll =
+      window.pageYOffset || document.documentElement.scrollTop;
 
-document.querySelectorAll(".menu-ancora a").forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
+    // Comportamento do header ao rolar
+    if (currentScroll > lastScrollTop) {
+      header.style.transform = "translateY(0)";
+    } else {
+      header.style.transform = "translateY(-100%)";
+    }
 
-    const targetId = this.getAttribute("href").substring(1);
-    const targetElement = document.getElementById(targetId);
-    const offsetPosition = targetElement.offsetTop - 140;
+    // Toggle da classe sticky-active
+    if (window.scrollY > 70) {
+      header.classList.add("sticky-active");
+    } else {
+      header.classList.remove("sticky-active");
+      header.style.transform = "translateY(0)";
+    }
 
-    window.scrollTo({
-      top: offsetPosition,
-      behavior: "smooth",
+    updateMenuActiveState();
+    lastScrollTop = Math.max(currentScroll, 0);
+  });
+
+  updateMenuActiveState();
+}
+
+function initPhoneInputMasking() {
+  const input = document.querySelector("#phone");
+  window.intlTelInput(input, {
+    onlyCountries: ["br", "us", "ar", "uy", "py"],
+    initialCountry: "br",
+    preferredCountries: [],
+    separateDialCode: true,
+    utilsScript:
+      "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+  });
+}
+
+function initSmoothScrollForMenuLinks() {
+  document.querySelectorAll(".menu-ancora a").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+      const offsetPosition = targetElement.offsetTop - 140;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
     });
   });
-});
+}
 
-document.addEventListener("DOMContentLoaded", function () {
+function initCarousel() {
   const itemsContainer = document.querySelector(".service-items");
   const dots = document.querySelectorAll(".dot");
   let currentIndex = 0;
@@ -91,162 +118,119 @@ document.addEventListener("DOMContentLoaded", function () {
   function startAutoSlide() {
     autoSlideInterval = setInterval(() => {
       if (!userInteracted) {
-        // Verifica se o usuário não interagiu manualmente
-        if (currentIndex < dots.length - 1) {
-          showSlide(currentIndex + 1);
-        } else {
-          showSlide(0);
-        }
+        showSlide((currentIndex + 1) % dots.length);
       }
-    }, 10000); // Avança a cada 10 segundos
-  }
-
-  function resetAutoSlide() {
-    if (!userInteracted) {
-      // Reinicia o temporizador apenas se não houve interação manual
-      clearInterval(autoSlideInterval);
-      startAutoSlide();
-    }
+    }, 10000);
   }
 
   dots.forEach((dot, index) => {
     dot.addEventListener("click", () => {
       showSlide(index);
-      userInteracted = true; // Marca que houve interação manual
-      clearInterval(autoSlideInterval); // Para o auto slide
+      userInteracted = true;
+      clearInterval(autoSlideInterval);
     });
   });
 
-  // Navegação por gestos (swipe)
   let startX = 0;
 
-  itemsContainer.addEventListener("touchstart", function (e) {
+  itemsContainer.addEventListener("touchstart", (e) => {
     startX = e.touches[0].clientX;
   });
 
-  itemsContainer.addEventListener("touchend", function (e) {
-    let endX = e.changedTouches[0].clientX;
+  itemsContainer.addEventListener("touchend", (e) => {
+    const endX = e.changedTouches[0].clientX;
     if (startX > endX + 50) {
-      if (currentIndex < dots.length - 1) {
-        showSlide(currentIndex + 1);
-      } else {
-        showSlide(0); // Volta ao primeiro slide após o último
-      }
-      userInteracted = true; // Marca que houve interação manual
-      clearInterval(autoSlideInterval); // Para o auto slide
+      showSlide((currentIndex + 1) % dots.length);
+      userInteracted = true;
+      clearInterval(autoSlideInterval);
     } else if (startX < endX - 50) {
-      if (currentIndex > 0) {
-        showSlide(currentIndex - 1);
-      }
-      userInteracted = true; // Marca que houve interação manual
-      clearInterval(autoSlideInterval); // Para o auto slide
+      showSlide(currentIndex > 0 ? currentIndex - 1 : dots.length - 1);
+      userInteracted = true;
+      clearInterval(autoSlideInterval);
     }
   });
 
-  // Inicializa o carrossel e o temporizador se a largura da tela for de 768px ou menos (Mobile)
   if (window.innerWidth <= 768) {
     showSlide(currentIndex);
     startAutoSlide();
   }
-});
+}
 
-document.addEventListener("DOMContentLoaded", function () {
+function initHeroSectionAnimation() {
   const heroContainer = document.querySelector(".hero .container");
 
   if (window.innerWidth <= 768) {
-    // Verifica se o dispositivo é mobile
-    setTimeout(function () {
+    setTimeout(() => {
       heroContainer.classList.add("animate");
-    }, 100); // Pequeno atraso para garantir que a animação seja visível
-  }
-});
-
-const contentContainer = document.querySelector(".persuasive-section .content");
-function handleMouseMove(event) {
-  if (window.innerWidth > 768) {
-    const { clientX } = event;
-    const contentContainer = document.querySelector(".content"); // Certifique-se de selecionar o container correto
-    const containerWidth = contentContainer.offsetWidth;
-    const scrollWidth = contentContainer.scrollWidth;
-    const maxScroll = scrollWidth - containerWidth;
-
-    // Calcula a posição do mouse em relação ao centro do container
-    const centerX = containerWidth / 2;
-    const deltaX = clientX - centerX;
-
-    // Calcula a porcentagem de deslocamento (-1 a 1)
-    const percentage = deltaX / centerX;
-
-    // Calcula o scrollAmount de forma simétrica
-    let scrollAmount = (maxScroll / 2) * percentage + maxScroll / 2;
-
-    // Limita o scrollAmount entre 0 e maxScroll
-    scrollAmount = Math.max(0, Math.min(maxScroll, scrollAmount));
-
-    // Aplica a transformação para deslocar os itens suavemente
-    contentContainer.style.transform = `translateX(${-scrollAmount}px)`;
+    }, 100);
   }
 }
 
-// Adiciona o evento de movimento do mouse sobre a seção, mas só para desktop
-document
-  .querySelector(".persuasive-section")
-  .addEventListener("mousemove", handleMouseMove);
+function initParallaxEffect() {
+  const contentContainer = document.querySelector(
+    ".persuasive-section .content"
+  );
 
-// Variável para detectar quando o formulário foi submetido
-let submitted = false;
+  function handleMouseMove(event) {
+    if (window.innerWidth > 768) {
+      const { clientX } = event;
+      const containerWidth = contentContainer.offsetWidth;
+      const scrollWidth = contentContainer.scrollWidth;
+      const maxScroll = scrollWidth - containerWidth;
 
-document.addEventListener("DOMContentLoaded", function () {
+      const centerX = containerWidth / 2;
+      const deltaX = clientX - centerX;
+      const percentage = deltaX / centerX;
+
+      let scrollAmount = (maxScroll / 2) * percentage + maxScroll / 2;
+      scrollAmount = Math.max(0, Math.min(maxScroll, scrollAmount));
+
+      contentContainer.style.transform = `translateX(${-scrollAmount}px)`;
+    }
+  }
+
+  document
+    .querySelector(".persuasive-section")
+    .addEventListener("mousemove", handleMouseMove);
+}
+
+function initFormSubmissionHandling(header, contactSection) {
+  let submitted = false;
   const form = document.querySelector("form");
   const formGroups = document.querySelectorAll(".form-group");
   const submitButton = document.querySelector('button[type="submit"]');
   const iframe = document.getElementById("hidden_iframe");
-  const header = document.querySelector(".header-container");
-  const contactSection = document.getElementById("contato"); // Referência para a seção de contato
 
-  // Detecta o envio do formulário
-  form.addEventListener("submit", function () {
-    submitted = true; // Marca o formulário como enviado
+  form.addEventListener("submit", () => {
+    submitted = true;
   });
 
-  // Verifica quando o iframe termina de carregar (após o envio do formulário)
-  iframe.addEventListener("load", function () {
+  iframe.addEventListener("load", () => {
     if (submitted) {
-      // Captura a altura atual do formulário
       const initialFormHeight = form.offsetHeight;
 
-      // Esconde todos os grupos de formulário e o botão de envio
-      formGroups.forEach(function (group) {
+      formGroups.forEach((group) => {
         group.style.display = "none";
       });
       submitButton.style.display = "none";
 
-      // Cria a mensagem de sucesso dinamicamente
       const successMessage = document.createElement("h4");
       successMessage.id = "success-message";
-      successMessage.textContent = "Mensagem enviada com sucesso! \u2705";
+      successMessage.textContent = "Mensagem enviada com sucesso! ✅";
       successMessage.style.display = "flex";
 
-      // Insere a mensagem antes do fechamento da tag </form>
       form.appendChild(successMessage);
 
-      // Força o recálculo do layout para capturar a nova altura com o conteúdo da mensagem
       const newFormHeight = form.scrollHeight;
-
-      // Define a altura inicial antes de iniciar a transição
       form.style.height = `${initialFormHeight}px`;
 
-      // Permite um pequeno intervalo para garantir a transição de altura suave
       setTimeout(() => {
-        // Atualiza a altura do formulário para a nova altura, desencadeando a transição
         form.style.height = `${newFormHeight}px`;
 
-        // Após a transição, opcionalmente definir a altura como auto para futuros ajustes
         setTimeout(() => {
           form.style.height = "auto";
-        }, 1000); // O mesmo tempo da transição de altura definida no CSS (1s)
+        }, 1000);
 
-        // Rola a tela suavemente para o topo da seção #contato, considerando a altura do header
         const headerHeight = header.offsetHeight;
         const sectionPosition =
           contactSection.getBoundingClientRect().top + window.scrollY;
@@ -256,17 +240,15 @@ document.addEventListener("DOMContentLoaded", function () {
           top: offsetPosition,
           behavior: "smooth",
         });
-      }, 100); // Intervalo para garantir que a nova altura seja aplicada
+      }, 100);
     }
   });
-});
+}
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Verifica se a tela é menor que 768px (mobile)
+function initImageTogglingOnMobile() {
   const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
   if (isMobile) {
-    // Seletor de todos os pontos do produto
     const points = document.querySelectorAll(".point");
 
     points.forEach((point) => {
@@ -274,117 +256,85 @@ document.addEventListener("DOMContentLoaded", function () {
       const mainImg = point.querySelector(".main-img");
       const altImg = point.querySelector(".alt-img");
 
-      // Função para alternar as imagens
       function toggleImage() {
-        if (mainImg.style.display === "none") {
-          mainImg.style.display = "block";
-          altImg.style.display = "none";
-        } else {
-          mainImg.style.display = "none";
-          altImg.style.display = "block";
-        }
+        const isMainVisible = mainImg.style.display !== "none";
+        mainImg.style.display = isMainVisible ? "none" : "block";
+        altImg.style.display = isMainVisible ? "block" : "none";
       }
 
-      // Configura a troca automática de imagens a cada 5 segundos
       interval = setInterval(toggleImage, 5000);
 
-      // Troca de imagem manual ao clicar no card da imagem
-      point.addEventListener("click", function () {
+      point.addEventListener("click", () => {
         toggleImage();
-        // Reseta o intervalo para garantir que ele continua trocando depois da interação manual
         clearInterval(interval);
         interval = setInterval(toggleImage, 5000);
       });
     });
   }
-});
+}
 
-document.addEventListener("DOMContentLoaded", function () {
+function initWhatsAppButtonVisibility() {
   const homeSection = document.getElementById("home");
   const whatsappButton = document.querySelector(".whatsapp-button");
 
-  // Função que será chamada quando o elemento for intersectado
   const observer = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          // Se a seção #home estiver visível, escondemos o botão
-          whatsappButton.classList.remove("visible");
-        } else {
-          // Se a seção #home estiver fora da tela, mostramos o botão
-          whatsappButton.classList.add("visible");
-        }
+        whatsappButton.classList.toggle("visible", !entry.isIntersecting);
       });
     },
     {
-      threshold: 0.95, // 10% da seção #home visível ainda conta como "visível"
+      threshold: 0.95,
     }
   );
 
-  // Observa a seção #home
   observer.observe(homeSection);
-});
+}
 
-document.addEventListener("DOMContentLoaded", function () {
+function initHighlightAnimation() {
   const highlights = document.querySelectorAll(".highlight, .highlight-blue");
 
   const observer = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          // Executa o efeito apenas quando o highlight entra na tela
           const highlight = entry.target;
+          const backgroundColor = "#ffc10780";
 
-          // Define a cor do destaque com base na classe
-          let backgroundColor = "#ffc10780"; // Cor padrão para 'highlight'
-          if (highlight.classList.contains("highlight-blue")) {
-            backgroundColor = "#ffc10780"; // Cor para 'highlight-blue'
-          }
-
-          // Define a posição do highlight
           highlight.style.position = "relative";
 
-          // Cria um pseudo-elemento ::before via JavaScript
           const beforeElement = document.createElement("span");
-
           beforeElement.style.position = "absolute";
           beforeElement.style.top = "0";
           beforeElement.style.left = "0";
-          beforeElement.style.width = "100%"; // O fundo cobrirá todo o conteúdo do highlight
+          beforeElement.style.width = "100%";
           beforeElement.style.height = "100%";
-          beforeElement.style.zIndex = "-1"; // Fica atrás do texto
+          beforeElement.style.zIndex = "-1";
           beforeElement.style.backgroundColor = backgroundColor;
           beforeElement.style.transition =
-            "clip-path 1s ease-out, opacity 1s ease-out"; // Suavidade da transição
-
-          // Inicialmente, o fundo será cortado à esquerda
+            "clip-path 1s ease-out, opacity 1s ease-out";
           beforeElement.style.clipPath = "inset(0 100% 0 0)";
 
-          // Insere o pseudo-elemento antes do conteúdo do highlight
           highlight.appendChild(beforeElement);
 
-          // Inicia o efeito de "marcatexto" após um pequeno atraso
           setTimeout(() => {
-            beforeElement.style.clipPath = "inset(0 0 0 0)"; // O fundo aparece da esquerda para a direita
+            beforeElement.style.clipPath = "inset(0 0 0 0)";
 
-            // Após a transição, suaviza a remoção do fundo
             setTimeout(() => {
-              beforeElement.style.opacity = "0"; // Suaviza o desaparecimento do fundo
-            }, 500); // Tempo para o fundo desaparecer
-          }, 500); // 500ms de atraso para iniciar o efeito quando o elemento entra na tela
+              beforeElement.style.opacity = "0";
+            }, 500);
+          }, 500);
 
-          // Desativa o observador para que o efeito só aconteça uma vez
           observer.unobserve(highlight);
         }
       });
     },
     {
-      threshold: 0.5, // 50% do elemento precisa estar visível para acionar o efeito
+      threshold: 0.5,
     }
   );
 
-  // Observa cada highlight e highlight-blue
   highlights.forEach((highlight) => {
     observer.observe(highlight);
   });
-});
+}
